e96cfac0a2cd23ae99d1d3fd76ba25047e0b03fc
Christopher S. Wise <csw@foundationsoft.com>
2020-03-04 16:44:57 -0500

    Initial Push

---
+++ script/script.js
@@ -0,0 +1,15 @@
+$('span').toggle(
+
+function() {
+$('#main').css('left', '250px')
+},
+function() {
+$('#main').css('left', '0')
+})
+/*
+function openSlideMenu() {
+document.getElementById('side-menu').style.width = '250px';
+document.getElementById('main').style.marginLeft = '250px';
+document.getElementById('navbar').style.marginLeft = '250px';
+}
+*/
